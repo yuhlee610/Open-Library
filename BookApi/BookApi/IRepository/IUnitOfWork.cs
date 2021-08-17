47af56f4327752e58c95e287ed07b6e1b70fb9ee
yuhlee610 <yuhlee610@gmail.com>
2021-08-17 20:50:15 +0700

    Use pattern Unit of Work

---
+++ BookApi/BookApi/IRepository/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using BookApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.IRepository
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<User> Users { get; }
+        IGenericRepository<Book> Books { get; }
+        IGenericRepository<Category> Categories { get; }
+        Task Save();
+    }
+}
---
+++ BookApi/BookApi/Repository/UnitOfWork.cs
@@ -0,0 +1,79 @@
+﻿using BookApi.Data;
+using BookApi.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.Repository
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly ApplicationDbContext _context;
+        IGenericRepository<User> _users;
+        IGenericRepository<Book> _books;
+        IGenericRepository<Category> _categories;
+        private bool disposed = false;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<User> Users
+        {
+            get
+            {
+                if(_users == null)
+                {
+                    _users = new GenericRepository<User>(_context);
+                }
+                return _users;
+            }
+        }
+
+        public IGenericRepository<Book> Books
+        {
+            get
+            {
+                if (_books == null)
+                {
+                    _books = new GenericRepository<Book>(_context);
+                }
+                return _books;
+            }
+        }
+
+        public IGenericRepository<Category> Categories
+        {
+            get
+            {
+                if (_categories == null)
+                {
+                    _categories = new GenericRepository<Category>(_context);
+                }
+                return _categories;
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
