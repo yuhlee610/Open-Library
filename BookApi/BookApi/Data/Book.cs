cfb6049121316c2f82db58da90d51e0221de035c
yuhlee610 <yuhlee610@gmail.com>
2021-08-17 10:10:36 +0700

    Initalize BookApi, create Models

---
+++ BookApi/BookApi/Data/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.Data
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Picture { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
